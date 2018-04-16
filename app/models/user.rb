2ae0b70711534df03f1f1b0a2f95d3e115860e96
Dino <dinogaric1@gmail.com>
2018-04-16 11:31:13 +0000

    matic je bog

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,18 +21,17 @@ class User < ActiveRecord::Base
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
-        user.name = auth.info.name   # assuming the user model has a namegit c
+        user.name = auth.info.name   # assuming the user model has a name
         # If you are using confirmable and the provider(s) you use validate emails, 
         # uncomment the line below to skip the confirmation emails.
         # user.skip_confirmation!
       end
     end
     
-def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
+     def facebook
+    if request.env["omniauth.auth"].info.email.blank?
+      redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
+      return # be sure to include an return if there is code after this otherwise it will be executed
     end
   end
     
